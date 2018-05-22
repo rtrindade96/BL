1ac78447d9175f08e4e5ebd55aef5d6dd0938edf
Ranielle Lucio Trindade <raniellelucio@gmail.com>
2018-05-22 12:12:46 -0300

    Alterações para o app começar a consumir dados do sw

---
+++ app/src/main/java/com/example/ranielle/baronlogger/Helper/Constants.java
@@ -0,0 +1,9 @@
+package com.example.ranielle.baronlogger.Helper;
+
+public class Constants {
+
+    public static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+    public static final String TOKEN = "token";
+    public static final String EMAIL = "email";
+
+}
---
+++ app/src/main/java/com/example/ranielle/baronlogger/Model/Response.java
@@ -0,0 +1,17 @@
+package com.example.ranielle.baronlogger.Model;
+
+public class Response {
+
+        private String message;
+        private String token;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+
+}
