fd008d49be5852c13bb0ebb14d3d4f361cf7af6a
heitormasson <heitormasson@gmail.com>
2018-05-22 19:25:42 -0300

    Tela de cadastro adicionada

---
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.example.ranielle.baronlogger.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ranielle.baronlogger.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
