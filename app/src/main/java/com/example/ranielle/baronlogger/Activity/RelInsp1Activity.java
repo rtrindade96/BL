573331bdd74dc67c25c29aa47a7c0cc48474ca10
heitormasson <heitormasson@gmail.com>
2018-05-22 21:45:49 -0300

    Add tela revestimento e tela de inspecao

---
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/RelInsp1Activity.java
@@ -0,0 +1,15 @@
+package com.example.ranielle.baronlogger.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ranielle.baronlogger.R;
+
+public class RelInsp1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rel_insp1);
+    }
+}
