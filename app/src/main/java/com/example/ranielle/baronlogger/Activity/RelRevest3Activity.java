438044da4a79e5ccb2d4aebf85e57b822e19a95d
heitormasson <heitormasson@gmail.com>
2018-05-27 18:56:41 -0300

    Telas revestimento

---
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest3Activity.java
@@ -0,0 +1,13 @@
+package com.example.ranielle.baronlogger.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelRevest3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rel_revest3);
+    }
+}
