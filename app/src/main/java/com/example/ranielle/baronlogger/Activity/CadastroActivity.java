c9d5815bc28e38d612c5a9efa75f0658ea8504c2
heitormasson <heitormasson@gmail.com>
2018-05-30 15:08:43 -0300

    correções de sintaxe e id tela de revestimento e activity cadastro

--- app/src/main/java/com/example/ranielle/baronlogger/Activity/CadastroActivity.java
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/CadastroActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.ranielle.baronlogger.MainActivity;
 import com.example.ranielle.baronlogger.R;
@@ -15,11 +17,25 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        Spinner dropdown = findViewById(R.id.spinnerFABRICANTE);
+        Spinner dropdown2 = findViewById(R.id.spinnerTIPOEQ);
+        //create a list of items for the spinner.
+        String[] itemsFABRICANTE = new String[]{"CATERPILLAR"};
+        String[] itemsTIPOEQ = new String[]{"FORA DE ESTRADA"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapterFAB = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsFABRICANTE);
+        ArrayAdapter<String> adapterTIPO = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item, itemsTIPOEQ);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapterFAB);
+        dropdown2.setAdapter(adapterTIPO);
 
         Button avancar = findViewById(R.id.bntCONCLUIR);
         avancar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 Intent intentVaipraMain = new Intent(CadastroActivity.this, MainActivity.class);
                 startActivity(intentVaipraMain);
             }
--- app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest3Activity.java
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest3Activity.java
@@ -2,6 +2,7 @@ package com.example.ranielle.baronlogger.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.ranielle.baronlogger.R;
 
 public class RelRevest3Activity extends AppCompatActivity {
 
