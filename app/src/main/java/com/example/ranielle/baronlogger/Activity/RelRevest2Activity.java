a04ede6d693dd4fe1fd71e3bd7534780858ec765
heitormasson <heitormasson@gmail.com>
2018-06-20 15:13:34 -0300

    Atualizacoes activitys de revestimento

--- app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest2Activity.java
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest2Activity.java
@@ -1,7 +1,10 @@
 package com.example.ranielle.baronlogger.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ranielle.baronlogger.R;
 
@@ -11,5 +14,17 @@ public class RelRevest2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rel_revest2);
+
+        Button avancar = findViewById(R.id.bntAVANÇAR);
+        avancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //GUARDAR OS DADOS inspetor, CLIENTE, TAG, DataInst e DataRev
+                Intent intentVaipraTerPag = new Intent(RelRevest2Activity.this, RelRevest3Activity.class);
+                startActivity(intentVaipraTerPag);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest3Activity.java
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/RelRevest3Activity.java
@@ -1,7 +1,11 @@
 package com.example.ranielle.baronlogger.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.ranielle.baronlogger.R;
 
 public class RelRevest3Activity extends AppCompatActivity {
@@ -10,5 +14,15 @@ public class RelRevest3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rel_revest3);
+
+        Button concluir = findViewById(R.id.bntCONCLUIR);
+        concluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //GUARDAR OS DADOS inspetor, CLIENTE, TAG, DataInst e DataRev
+                finish();
+            }
+        });
     }
 }
