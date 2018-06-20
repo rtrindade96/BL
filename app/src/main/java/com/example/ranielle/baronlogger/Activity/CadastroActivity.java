4285c94a7ee52ffd766c83b63b5644b0c8204596
Ranielle Lucio Trindade <raniellelucio@gmail.com>
2018-06-20 15:24:43 -0300

    Merge branch 'master' of https://github.com/rtrindade96/BL
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/ranielle/baronlogger/Activity/CadastroActivity.java
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/CadastroActivity.java
@@ -1,16 +1,13 @@
 package com.example.ranielle.baronlogger.Activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 
-import com.example.ranielle.baronlogger.MainActivity;
 import com.example.ranielle.baronlogger.R;
 
 public class CadastroActivity extends AppCompatActivity {
--- app/src/main/java/com/example/ranielle/baronlogger/Activity/PrincipalActivity.java
+++ app/src/main/java/com/example/ranielle/baronlogger/Activity/PrincipalActivity.java
@@ -1,9 +1,7 @@
 package com.example.ranielle.baronlogger.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -34,7 +32,10 @@ public class PrincipalActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
+
 
     @Override
     public void onBackPressed() {
@@ -74,18 +75,19 @@ public class PrincipalActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.btrelrevestimento) {
+                Intent intent = new Intent(this,  RelRevest1Activity.class);
+                startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.btcadequipamento) {
+            Intent intent = new Intent(this,  CadastroActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.btlogout) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.btrelinsprevestimento) {
+            Intent intent = new Intent(this,  RelInsp1Activity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
